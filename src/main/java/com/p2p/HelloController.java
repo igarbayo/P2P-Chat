1430ec70b8beff33ffffb709a61f9bc33125a1c1
iggarbayo <iggarbayo@gmail.com>
2024-11-14 17:30:52 +0100

    esquema

--- src/main/java/com/p2p/HelloController.java
+++ src/main/java/com/p2p/HelloController.java
@@ -1,11 +1,15 @@
 package com.p2p;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Button boton;
 
     // comentario
 
