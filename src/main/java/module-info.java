fb668e47cff4142fc4745007551072fc2876d2e5
iggarbayo <iggarbayo@gmail.com>
2024-11-18 20:06:06 +0100

    arreglado module-info para poder compilar. sobreescritura de com.Client

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,13 @@ module com.Server {
     requires javafx.fxml;
     requires java.rmi;
 
-
+    // Permite la reflexión para el paquete com.Server
     opens com.Server to javafx.fxml;
+
+    // Exporta com.Server para uso general
     exports com.Server;
+
+    // Agrega estas líneas para manejar com.Client
+    opens com.Client to javafx.fxml; // Permite reflexión en com.Client
+    exports com.Client;             // Exporta com.Client para otros módulos
 }
